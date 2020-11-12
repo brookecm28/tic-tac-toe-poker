37ce2f25412b6e1931d63de70fa93b228049e742
Brooke Miller <brookecm28@gmail.com>
2020-11-12 08:40:43 -0700

    Poker Complete

--- Part2/index.js
+++ Part2/index.js
@@ -1,3 +1,17 @@
-console.log("im in here")
+console.log("The house always wins!")
 
+const id = document.getElementById('identification')
+console.log(id)
+const colorInput = document.getElementById('style')
+console.log(style)
 
+function setCard () {
+   const card = document.getElementById(id.value)
+   console.log(card)
+   card.style.color = colorInput.value
+}
+
+const refresh = document.getElementById('refresh')
+refresh.addEventListener('click', e => {
+    window.location.reload(false)
+})
